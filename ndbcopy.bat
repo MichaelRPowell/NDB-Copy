a5ff6f37be4fd3b5107cb4c61fdd83ee9215c53e
Michael R. Powell <michaelrpowell@gmail.com>
2024-03-29 13:30:58 -0400

    Update ndbcopy.bat

--- ndbcopy.bat
+++ ndbcopy.bat
@@ -1,9 +1,12 @@
 ::Backup NDB Files
 robocopy \\remote\NDB D:\NDB.Backups\Remote /MIR /W:30 /R:5000
 robocopy C:\Program\NDB D:\NDB.Backups\Local /MIR /W:30 /R:5000
+timeout /t 30
 
 ::Copy NDB Files to Remote
 robocopy D:\NDB \\remote\NDB /MIR /W:30 /R:5000
+timeout /t 30
 
 ::Copy NDB Files to Local
 robocopy D:\NDB C:\Program\NDB /MIR /W:30 /R:5000
+timeout /t 30
