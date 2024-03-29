57561a7a9477112494da79daa6544e44347027a1
Michael R. Powell <michaelrpowell@gmail.com>
2024-03-29 13:34:59 -0400

    Update ndbcopy.bat

--- ndbcopy.bat
+++ ndbcopy.bat
@@ -1,5 +1,13 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%%HH%%Min%%Sec%"
+
+
 ::Backup NDB Files
-robocopy \\remote\NDB D:\NDB.Backup /MIR /W:30 /R:5000
+robocopy \\remote\NDB D:\NDB.Backup\%datestamp% /MIR /W:30 /R:5000
 timeout /t 30
 
 ::Copy NDB Files to Remote
