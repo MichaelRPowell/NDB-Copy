1ef7dfafea194e00d32a6ce07ed4e82fd1f0dd43
Michael R. Powell <michaelrpowell@gmail.com>
2024-03-29 13:35:49 -0400

    Update ndbcopy.bat

--- ndbcopy.bat
+++ ndbcopy.bat
@@ -5,6 +5,8 @@ set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%%HH%%Min%%Sec%"
 
+@echo on
+
 ::Backup NDB Files
 robocopy \\remote\NDB D:\NDB.Backup\%datestamp% /MIR /W:30 /R:5000
 timeout /t 30
